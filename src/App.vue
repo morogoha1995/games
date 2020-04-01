0af680ebbdf7263a3948c1106ce06aaafc5411e1
moro <62976395+morogoha1995@users.noreply.github.com>
2020-04-01 13:49:18 +0900

    version one.

---
+++ src/App.vue
@@ -0,0 +1,127 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+        <v-subheader>カテゴリー</v-subheader>
+        <v-list-item-group v-model="selectedJanre" color="teal">
+          <v-list-item @click="getAll()" link>
+            <v-list-item-content>
+              <v-list-item-title>すべて</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item v-for="j in janres" :key="j.name" @click="getGames(j.id)" link>
+            <v-list-item-content>
+              <v-list-item-title>{{j.name}}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+        <v-divider />
+        <v-list-item :href="config.base_url" link>
+          <v-list-item-content>
+            <v-list-item-title>開発者BLOG</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="orange" dark clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <div class="d-flex align-center">{{config.site_name}}</div>
+    </v-app-bar>
+
+    <v-content>
+      <v-container>
+        <v-row>
+          <v-col v-for="g in games" :key="g.name" v-show="g.isShow" cols="12" sm="6" md="4" lg="3">
+            <v-card>
+              <v-img :src="getImgSrc(g.url)" :alt="getImgAlt(g.name)" height="180px"></v-img>
+              <v-card-title>{{g.name}}</v-card-title>
+              <v-card-subtitle>{{ getJanreName(g.janre_id) }}</v-card-subtitle>
+              <v-card-text>{{g.description}}</v-card-text>
+
+              <v-card-actions>
+                <v-btn :href="getUrl(g.url)" outlined dark color="teal">遊ぶ</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import _informations from "./informations.json";
+import janres from "./janres.json";
+import config from "./config.json";
+
+// isShowの初期値はtrue。jsonファイルに書いてもいいけどゲーム数が膨大になると冗長なのでここで全てにisShow=trueをしておく。
+let informations = [];
+let gameCount = 0;
+for (let i of _informations) {
+  gameCount++;
+  i.isShow = true;
+  informations.push(i);
+}
+
+export default {
+  name: "App",
+
+  data: () => ({
+    gameCount: gameCount,
+    games: informations,
+    janres: janres,
+    config: config,
+    drawer: false,
+    selectedJanre: 0
+  }),
+
+  methods: {
+    getAll: function() {
+      let gameCount = 0;
+      for (let i = 0; i < this.games.length; i++) {
+        gameCount++;
+        this.games[i].isShow = true;
+      }
+      this.gameCount = gameCount;
+    },
+
+    getGames: function(janreID) {
+      let gameCount = 0;
+      for (let i = 0; i < this.games.length; i++) {
+        if (this.games[i].janre_id === janreID) {
+          gameCount++;
+          this.games[i].isShow = true;
+        }
+      }
+      this.gameCount = gameCount;
+    },
+
+    getJanreName: function(janreID) {
+      for (const j of this.janres) {
+        if (j.id === janreID) {
+          return j.name;
+        }
+      }
+    },
+
+    getUrl: function(url) {
+      return `${this.config.base_url}/games/${url}`;
+    },
+
+    getImgSrc: function(url) {
+      return `${this.config.base_url}/games/imgs/${url}.png`;
+    },
+
+    getImgAlt: function(name) {
+      return `${name}のサムネイル`;
+    }
+  }
+};
+</script>
+
+<style>
+.v-application {
+  font-family: Meiryo, sans-serif;
+}
+</style>
